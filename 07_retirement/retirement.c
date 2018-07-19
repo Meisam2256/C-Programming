300d0b242bcc59d8dd4625cfbfa6d715b8f179da
Coursera Learner <nobody@nowhere.nul>
2018-07-19 04:29:11 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -5,7 +5,7 @@
 struct _retire_info{
   int months;//number of the months
   double contribution;//how many dollars are contributed from the account per month
-double rate_of_return;// rate of return which is for after inflation
+  double rate_of_return;// rate of return which is for after inflation
 };
 typedef struct _retire_info retire_info;
 
@@ -15,18 +15,44 @@ void retirement(int startAge,// in months
                 retire_info retired //info about being retired
                 ){
   double balance;
-  balance  = (initial * working.rate_of_return) + working.contribution;
-  if(startAge%12 == 0){
-  printf("Age %3d month %2d you have $%.21f\n", startAge,0,  balance);
-}
-  else{
-    int Num_Month = startAge%12;
-  printf("Age %3d month %2d you have $%.21f\n", startAge,Num_Month,  balance);
-    
-  }
-}
-  int main(){
-    int startAge = 327;
-    int initial = 21345;
+  int i;
+  //startAge = 327;
+  //initial = 21345;
+  printf("Age %3d month %2d you have $%.21f\n", startAge/12 , startAge%12 ,initial);
   
+  balance  = (initial * working.rate_of_return) + working.contribution;
+  for( i  = startAge ; i  <= (startAge + working.months) ;  i++){
+    printf("Age %3d month %2d you have $%.21f\n", startAge/12,startAge%12 , balance);
   }
+  
+  balance  = (balance * retired.rate_of_return) + retired.contribution;
+  for( i  = (startAge+working.months) ; startAge< startAge + working.months + retired.months ; i++){
+    printf("Age %3d month %2d you have $%.21f\n", startAge/12, startAge%12,  balance);
+  }
+  return ;
+  }
+
+
+  int main(void){
+    int startAge = 327;
+    double initial = 21345;
+    //retirement (initial = 21345);
+    //retirement(startAge, initial, working, retired);
+    retire_info working ;
+    //working.startAge = 327;
+    //working.initial = 21345;
+    working.months = 489;
+    working.contribution = 1000;
+    working.rate_of_return = 0.045/12;
+    //retirement(working, retired);
+    //for(working.startAge =0; working.startAge<489;  working.startAge){
+    //balance =
+    
+    retire_info retired ;
+    retired.months = 384;
+    retired.contribution = -4000;
+    retired.rate_of_return= 0.01/12;
+    retirement(startAge, initial, working, retired);
+  }
+
+
