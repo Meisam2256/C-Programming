beb626d1b52dbb6aa1ece30075cb1b43ff7fcdaf
Coursera Learner <nobody@nowhere.nul>
2018-07-21 00:16:42 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,10 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 
 struct _retire_info{
   int months;//number of the months
   double contribution;//how many dollars are contributed from the account per month
-  double rate_of_return;// rate of return which is for after inflation
+double rate_of_return;// rate of return which is for after inflation
 };
 typedef struct _retire_info retire_info;
 
@@ -14,41 +13,34 @@ void retirement(int startAge,// in months
                 retire_info working,//info about working
                 retire_info retired //info about being retired
                 ){
-  double balance;
-  int i;
-  printf("Age %3d month %2d you have $%.2f\n", (startAge/12) , (startAge%12) ,initial);
-  
-  //balance  = (initial * working.rate_of_return) + working.contribution;
-  for( i  = startAge ; i  <= (startAge + working.months) ;  i++){
-    balance  = ((balance * working.rate_of_return) + working.contribution);
-    printf("Age %3d month %2d you have $%.2f\n", startAge/12,startAge%12 , balance);
+  double balance = initial;
+  int i=1;
+  printf("Age %3d month %2d you have $%.2f\n", (startAge/12) , (startAge%12) ,balance);
+
+  for( i  = startAge + i  ; i  <= (startAge + working.months) ;  i++){
+    balance  += ((balance * working.rate_of_return) + working.contribution) ;
+    printf("Age %3d month %2d you have $%.2f\n", i/12, i%12 , balance);
   }
   
-  //balance  = (balance * retired.rate_of_return) + retired.contribution;
-  for( i  = (startAge+working.months) ;  i < (startAge + working.months + retired.months) ; i++){
-    balance  = (balance * retired.rate_of_return) + retired.contribution;
+  for( i  = (startAge+working.months+ i) ;  i < (startAge + working.months + retired.months) ; i++){
+    balance  += ((balance * retired.rate_of_return) + retired.contribution);
     printf("Age %3d month %2d you have $%.2f\n", startAge/12, startAge%12,  balance);
   }
+
+
   //return ;
-  }
-  int main(void){
-       
-    //retirement (initial = 21345);
-    //retirement(startAge, initial, working, retired);
-    retire_info working ;
-    //retire_info startAge;
-    //working.initial = 21345;
-    working.months = 489;
-    working.contribution = 1000;
-    working.rate_of_return = 0.045/12;
-    //retirement(working, retired);
-    //for(working.startAge =0; working.startAge<489;  working.startAge){    
-    retire_info retired ;
-    retired.months = 384;
-    retired.contribution = -4000;
-    retired.rate_of_return= 0.01/12;
-    retirement(327, 21345 , working, retired);
-    return 0;
-  }
-
-
+}
+int main(void){
+  retire_info working ;
+   
+  working.months = 489;
+  working.contribution = 1000;
+  working.rate_of_return = (0.045/12);
+ 
+  retire_info retired ;
+  retired.months = 384;
+  retired.contribution = -4000;
+  retired.rate_of_return= (0.01/12);
+  retirement(327, 21345 , working, retired);
+  return 0;
+}
